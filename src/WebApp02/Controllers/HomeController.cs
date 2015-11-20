268d8a462241e94d866159ad6a5febe5b5722f07
madrus <madrus@gmail.com>
2015-11-20 20:24:13 +0100

    upgrade ASP.NET 6 beta8 to rc1-final

--- src/WebApp02/Controllers/HomeController.cs
+++ src/WebApp02/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-using Microsoft.Framework.OptionsModel;
+using Microsoft.Extensions.OptionsModel;
 using WebApp02.Models;
 
 namespace WebApp02.Controllers
